a8985ad643a9000f4d07deeeb60a314be1bd36cf
Manikanta <Manikanta@chinnureddy>
2025-09-14 10:27:52 +0530

    intial commit

---
+++ chinnu27/src/chinnu27/typeConversion.java
@@ -0,0 +1,12 @@
+package chinnu27;
+
+public class typeConversion {
+
+	public static void main(String[] args) {
+		double x = 20.23;
+		int y = (int)x;
+		System.out.println("int: " +y);
+
+	}
+
+}
